e34e05d2282671958919407fcaa3d614da193853
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-12 06:18:36 +0530

    ADDED: append Method to Return Key at Last

--- src/test/java/com/linkedlist/TestLinkedList.java
+++ src/test/java/com/linkedlist/TestLinkedList.java
@@ -3,7 +3,7 @@ package com.linkedlist;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TestLInkedList {
+public class TestLinkedList {
 
     @Test
     public void given3IntegersAddedToLinkedListShouldReturnToTop(){
@@ -20,5 +20,21 @@ public class TestLInkedList {
                 linkedList.tail.equals(firstNode);
         Assertions.assertTrue(result);
 
+    }
+
+    @Test
+    public void given3IntegersAddedToLinkedListShouldBeLastReturnTrue(){
+        Node<Integer> firstNode = new Node<>(56);
+        Node<Integer> secondNode = new Node<>(30);
+        Node<Integer> thirdNode = new Node<>(70);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.append(firstNode);
+        linkedList.append(secondNode);
+        linkedList.append(thirdNode);
+        boolean result = linkedList.head.equals(firstNode) &&
+                linkedList.head.getNextNode().equals(secondNode) &&
+                linkedList.tail.equals(thirdNode);
+        Assertions.assertTrue(result);
+        linkedList.printNode();
     }
 }
--- src/test/java/com/linkedlist/TestNode.java
+++ src/test/java/com/linkedlist/TestNode.java
@@ -16,20 +16,4 @@ public class TestNode {
                 secondNode.getNextNode().equals(thirdNode);
         Assertions.assertTrue(result);
     }
-
-    @Test
-    public void given3IntegersAddedToLinkedListShouldBeLastReturnTrue(){
-        Node<Integer> firstNode = new Node<>(56);
-        Node<Integer> secondNode = new Node<>(30);
-        Node<Integer> thirdNode = new Node<>(70);
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.append(firstNode);
-        linkedList.append(secondNode);
-        linkedList.append(thirdNode);
-        boolean result = linkedList.head.equals(firstNode) &&
-                linkedList.head.getNextNode().equals(secondNode) &&
-                linkedList.tail.equals(thirdNode);
-        Assertions.assertTrue(result);
-        linkedList.printNode();
-    }
 }
